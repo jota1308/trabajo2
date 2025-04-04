54465cca64c8a3469a90ec980f56f3f1686093c5
Joaquin Sciarretta <joaquinsciarretta@gmail.com>
2025-04-04 17:42:33 -0300

    creo el txt para instalar el notebook y arranco a escribir el readme para instalar entorno virtual y dependencias

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Joaquin Sciarretta dni:46908692
+pasos a seguir:
+1.es recomendable instalar un entorno virtual con el siguiente comando:
+    python -m venv (nombre del entorno virtual)
+2.para activar el entorno virtual se utiliza el siguiente comando:
+    (nombre del entorno)/scripts/activate
---
+++ notebooks/.ipynb_checkpoints/punto10-checkpoint.py
@@ -0,0 +1,40 @@
+from src import procesar
+
+rondas = [{
+'Shadow': {'kills': 2, 'assists': 1, 'deaths': True},
+'Blaze': {'kills': 1, 'assists': 0, 'deaths': False},
+'Viper': {'kills': 1, 'assists': 2, 'deaths': True},
+'Frost': {'kills': 0, 'assists': 1, 'deaths': False},
+'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}
+},
+{
+'Shadow': {'kills': 0, 'assists': 2, 'deaths': False},
+'Blaze': {'kills': 2, 'assists': 0, 'deaths': True},
+'Viper': {'kills': 1, 'assists': 1, 'deaths': False},
+'Frost': {'kills': 2, 'assists': 1, 'deaths': True},
+'Reaper': {'kills': 0, 'assists': 1, 'deaths': False}
+},
+{
+'Shadow': {'kills': 1, 'assists': 0, 'deaths': False},
+'Blaze': {'kills': 2, 'assists': 2, 'deaths': True},
+'Viper': {'kills': 1, 'assists': 1, 'deaths': True},
+'Frost': {'kills': 0, 'assists': 1, 'deaths': False},
+'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}
+},
+{
+'Shadow': {'kills': 2, 'assists': 1, 'deaths': False},         #rondas de la partida
+'Blaze': {'kills': 1, 'assists': 0, 'deaths': True},
+'Viper': {'kills': 0, 'assists': 2, 'deaths': False},
+'Frost': {'kills': 1, 'assists': 1, 'deaths': True},
+'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}
+},
+{
+'Shadow': {'kills': 1, 'assists': 2, 'deaths': True},
+'Blaze': {'kills': 0, 'assists': 1, 'deaths': False},
+'Viper': {'kills': 2, 'assists': 0, 'deaths': True},
+'Frost': {'kills': 1, 'assists': 1, 'deaths': False},
+'Reaper': {'kills': 1, 'assists': 1, 'deaths': True}
+}]
+
+
+partida = procesar.procesarpartida(rondas)         #llamamos a la funcion para procesar la partida y guardamos la partida en la varaible partida
